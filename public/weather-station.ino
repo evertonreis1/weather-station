bedbe4cc396cf2895b3e5d6a09fd4ea9f15e6fc4
Danilo Almeida <cs.daniloalmeida@gmail.com>
2023-10-02 18:38:16 -0300

    change html

--- public/weather-station.ino
+++ public/weather-station.ino
@@ -51,65 +51,107 @@ String readDHTHumidity() {
 }
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP32 DHT Server</h2>
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">&percnt;</sup>
-  </p>
-</body>
-<script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 10000 ) ;
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@700&display=swap" rel="stylesheet">
+    <style>
+      body {
+        background-color: #edf2f4;
+        color: #2b2d42;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        flex-direction: column;
+        text-align: center;
+        margin: 0px auto;
+        font-family: Arial, Helvetica, sans-serif
+      }
 
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 10000 ) ;
-</script>
+      h1 {
+        font-size: 3.0rem;
+        letter-spacing: 0.1em;
+        text-transform: uppercase;
+        margin-bottom: 20px;
+        font-family: 'Gabarito', 'Arial', sans-serif;
+        color: #1c1d2b;
+      }
+
+      p {
+        margin: 0;
+        font-size: 2rem;
+        color: #edf2f4;
+        text-align: center;
+        width: 100%;
+        line-height: 40px;
+      }
+
+      .p-wrapper {
+        background-color: #2b2d42;
+        border-radius: 20px;
+        margin: 20px;
+        padding: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 80%;
+        max-width: 600px;
+        box-shadow: 10px 10px 10px #8d99ae;
+      }
+
+      .p-wrapper i {
+        font-size: 3rem;
+        margin-right: 20px;
+      }
+    </style>
+
+  </head>
+  <body>
+    <h1>ESP32 DHT Server</h1>
+    <div class="p-wrapper">
+      <p>
+        <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+        <span class="dht-labels">Temperature:</span> 
+        <span id="temperature">%TEMPERATURE%<sup class="units">&deg;C</sup></span>
+      </p>
+    </div>
+    <div class="p-wrapper">
+      <p>
+        <i class="fas fa-tint" style="color:#00add6;"></i> 
+        <span class="dht-labels">Humidity:</span>
+        <span id="humidity">%HUMIDITY%<sup class="units">&percnt;</sup></span>
+      </p>
+    </div>
+  </body>
+
+  <script>
+    setInterval(function ( ) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("temperature").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "/temperature", true);
+      xhttp.send();
+    }, 10000 ) ;
+
+    setInterval(function ( ) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("humidity").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "/humidity", true);
+      xhttp.send();
+    }, 10000 ) ;
+  </script>
 </html>)rawliteral";
 
 // Replaces placeholder with DHT values
